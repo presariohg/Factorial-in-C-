be0a00b1859b79b22b6bccbe74320fde60110ebe
presariohg <31841577+presariohg@users.noreply.github.com>
2017-10-18 21:55:43 +0700

    Update factorial.cpp
    using loop

--- factorial.cpp
+++ factorial.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 auto factorial(auto n){
-    if(n < 1){
-        return 1;
-    } else {
-        return n*factorial(n - 1);
+    long long result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
     }
+    return result;
 }
 
 int main()
